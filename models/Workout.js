bf42dcb72d8a471f6cade731efbc035f74ea476b
Jim Nguyen <jimnguyen220@gmail.com>
2021-04-11 14:45:15 -0500

    server running, model built, starting routes

---
+++ models/Workout.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Please enter 'resistance' or 'cardio'"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter your exercise name"
+            },
+            duration: {
+                type: Number,
+                required: "Please enter duration for exercise"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
