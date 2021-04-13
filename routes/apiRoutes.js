de31fb5a596d0a02e2ec1085c7ae0cbbe625bbce
Jim Nguyen <jimnguyen220@gmail.com>
2021-04-13 00:49:50 -0500

    saving changes to routes, html routes not all working yet

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout");
+const Workout = require("../models/workout");
 
-router.post("/api/workouts", ({body}, res) => {
+router.put("/api/workouts", ({body}, res) => {
     Workout.create(body)
     .then(dbWorkout => {
         res.json(dbWorkout)
@@ -11,15 +11,15 @@ router.post("/api/workouts", ({body}, res) => {
     });
 });
 
-router.post("/api/workouts/bulk", ({body}, res) => {
-    Workout.insertMany(body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    });
-});
+// router.put("/api/workouts/bulk", ({body}, res) => {
+//     Workout.insertMany(body)
+//     .then(dbWorkout => {
+//         res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//         res.status(400).json(err);
+//     });
+// });
 
 router.get("api/workouts", (req, res) => {
     Workout.find({})
@@ -30,6 +30,21 @@ router.get("api/workouts", (req, res) => {
     .catch(err => {
         res.status(400).json(err);
     });
+});
+
+router.get("/api/workouts/:id", (req, res) => {
+    Workout.findOne(
+        {
+            _id: (req.params.id)
+        },
+        (error, data) => {
+            if (error) {
+                res.send(error)
+            } else {
+                res.send(data);
+            }
+        }
+    );
 });
 
 module.exports = router;
