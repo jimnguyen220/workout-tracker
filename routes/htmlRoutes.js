79a75fb573e4a0cdf3f6d6b159c266bc1d7cb8ea
Jim Nguyen <jimnguyen220@gmail.com>
2021-04-13 19:55:41 -0500

    fixed html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,15 +3,15 @@ const app = express();
 const path = require("path");
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 
