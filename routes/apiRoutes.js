78f8de342b130f408319078606566fd21808b5f0
Jim Nguyen <jimnguyen220@gmail.com>
2021-04-15 00:23:18 -0500

    debugging

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models");
+const Workout = require("../models/workout");
 
 //get route to work with getLastWorkout function
 router.get("/api/workouts", (req, res) => {
